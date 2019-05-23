12efe19bc58a69f30e63be5a7fd6d212179b2d93
Adam Wróblewski <adam.wroblewski@minebest.com>
2019-05-23 10:34:14 +0200

    praca domowa sass

--- zadanie-domowe/zadanie-sass-przed/gulpfile.js
+++ zadanie-domowe/zadanie-sass-przed/gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass', () => {
-    return gulp.src('sass/**/*.scss')
+    return gulp.src('scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('sass/**/*.scss', gulp.series('sass'))
+    gulp.watch('scss/**/*.scss', gulp.series('sass'))
 });
